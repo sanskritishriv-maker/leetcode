3f751569342338a1f2483f60d87507db20e8c350
sanskritishriv-maker <sanskritishriv@gmail.com>
2026-02-27 01:44:07 +0530

    Time: 1 ms (98.54%), Space: 47.5 MB (60.11%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        for(int i:nums) sum+=i;
+        int lsum=0;
+        for(int i=0;i<nums.length;i++){
+            if(lsum==sum-lsum-nums[i]){
+                return i;
+                }
+                lsum+=nums[i];
+        }
+        return -1;
+
+
+        
+    }
+}
